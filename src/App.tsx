ed1eda6eb3c868e2a6294b5bd376a38f9b355066
dmfiel <dave@fiel.us>
2025-06-27 10:21:15 -0400

    initial template updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useContext } from "react";
-import "./App.css";
+import './App.css';
+import { useContext } from 'react';
 import ThemeProvider, {
   ThemeButton,
-  ThemeContext,
-} from "./context/ThemeContext";
+  ThemeContext
+} from './context/ThemeContext';
 
 function App() {
   return (
     <ThemeProvider>
-      {" "}
-      <ThemeWrapper />{" "}
+      <ThemeWrapper />
     </ThemeProvider>
   );
 }
@@ -19,11 +18,15 @@ function ThemeWrapper() {
   return (
     <div
       id="all"
-      className={`${theme} w-full h-full bg:white dark:bg-black text-black dark:text-white flex flex-col min-h-screen`}
+      className={`${theme} w-full h-full bg:white dark:bg-black text-black dark:text-white flex flex-col min-h-screen px-5 pt-5`}
     >
+      <header className="flex justify-between">
+        <div></div>
+        <h1 className="text-3xl font-bold mx-auto">Recipe Discovery</h1>
+        <ThemeButton />
+      </header>
       <main className="mx-auto my-5 flex-1 flex flex-col gap-5 items-center">
-        {" "}
-        <ThemeButton />{" "}
+        <p>Put content here</p>
       </main>
       <footer role="contentinfo">
         <a
@@ -32,9 +35,9 @@ function ThemeWrapper() {
           target="_blank"
           className="text-center text-xs text-gray-500"
         >
-          <p>Letter f icons created by rashedul.islam - Flaticon</p>{" "}
-        </a>{" "}
-      </footer>{" "}
+          <p>Letter f icons created by rashedul.islam - Flaticon</p>{' '}
+        </a>{' '}
+      </footer>{' '}
     </div>
   );
 }
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -27,18 +27,30 @@ function ThemeProvider({ children }: { children: ReactNode }) {
   );
 }
 
-export function ThemeButton() {
+export function ThemeButton({ size }: { size?: number }) {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <button
       onClick={toggleTheme}
-      className="min-w-[147px] dark:bg-yellow-400 dark:hover:bg-yellow-500 bg-gray-800 hover:bg-gray-700 py-1 px-2 rounded-md my-2 text-white dark:text-black cursor-pointer"
+      className="w-fit whitespace-nowrap
+ dark:bg-yellow-400 dark:hover:bg-yellow-500 bg-gray-800 hover:bg-gray-700 py-1 px-2 rounded-md my-2 text-white dark:text-black cursor-pointer"
+      title={theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
       aria-label={
         theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'
       }
     >
-      {theme === 'light' ? '🌙 Switch to Dark' : '☀️ Switch to Light'}
+      {size === 3
+        ? theme === 'light'
+          ? '🌙 Switch to Dark'
+          : '☀️ Switch to Light'
+        : size === 1
+        ? theme === 'light'
+          ? '🌙'
+          : '☀️'
+        : theme === 'light'
+        ? '🌙 Dark'
+        : '☀️ Light'}
     </button>
   );
 }
